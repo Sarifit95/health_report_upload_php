3c41a5db99315d4a99719b67b5ae40a9976034cb
sarifit95@ <saifit95@gmail.com>
2021-09-28 14:43:14 +0600

    add ImageUpload class with Image resige

---
+++ class/ImageUpload.php
@@ -0,0 +1,55 @@
+<?php
+
+
+class ImageUpload
+{
+
+
+    function imageResize($imageResourceId,$mwidth,$mheight, $width, $height) {
+        $targetWidth =$width;
+        $targetHeight =$height;
+        $targetLayer=imagecreatetruecolor($targetWidth,$targetHeight);
+        imagecopyresampled($targetLayer,$imageResourceId,0,0,0,0,$targetWidth,$targetHeight, $mwidth,$mheight);
+        return $targetLayer;
+    }
+    function imagesave($filename,$savefilename, $uploadfolder, $width, $height){
+
+        $file = $_FILES[$filename]['tmp_name'];
+        $sourceProperties = getimagesize($file);
+        $ext = pathinfo($_FILES[$filename]['name'], PATHINFO_EXTENSION);
+        $fileNewName = $savefilename."." . $ext;
+        $imageType = $sourceProperties[2];
+        $filewroot = "$uploadfolder/" . $fileNewName;
+        switch ($imageType) {
+            case IMAGETYPE_PNG:
+                $imageResourceId = imagecreatefrompng($file);
+                $targetLayer = $this->imageResize($imageResourceId,  $sourceProperties[0], $sourceProperties[1], $width, $height);
+                imagepng($targetLayer, $filewroot);
+                break;
+
+            case IMAGETYPE_GIF:
+                $imageResourceId = imagecreatefromgif($file);
+                $targetLayer = $this->imageResize($imageResourceId,  $sourceProperties[0], $sourceProperties[1], $width, $height);
+                imagegif($targetLayer, $filewroot);
+                break;
+
+            case IMAGETYPE_JPEG:
+                $imageResourceId = imagecreatefromjpeg($file);
+                $targetLayer = $this->imageResize($imageResourceId,  $sourceProperties[0], $sourceProperties[1], $width, $height);
+                imagejpeg($targetLayer, $filewroot);
+                break;
+
+            default:
+                echo "Invalid Image type.";
+                exit;
+                break;
+
+        }
+
+    }
+
+
+
+
+
+}
